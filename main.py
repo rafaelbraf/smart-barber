2aadda9ed1b10e3526cc7a441264c22eb1ee2200
rafaelbraf <rafabrunofelix@hotmail.com>
2024-02-16 19:17:28 -0300

    feat - Adiciona classe Barbearia e endpoint para buscar barbearias

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import psycopg2
 
 app = Flask(__name__)
+
+db_params = {
+    'dbname': 'barbearia-app',
+    'user': 'postgres',
+    'password': 'minha_senha',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+
+def get_db_connection():
+    return psycopg2.connect(**db_params)
 
 
 @app.route('/')
 def hello_world():
     return "<p>Hello World!</p>"
+
+
+@app.route('/barbearias', methods=['GET'])
+def get_barbearias():
+    connection = get_db_connection()
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM barbearias")
+            barbearias = cursor.fetchall()
+
+        barbearias_json = [{'id': barbearia[0],
+                            'cnpj': barbearia[1],
+                            'nome': barbearia[2],
+                            'endereco': barbearia[3],
+                            'email': barbearia[4],
+                            'telefone': barbearia[5]} for barbearia in barbearias]
+
+        return jsonify({'barbearias': barbearias_json})
+
+    finally:
+        connection.close()
---
+++ model/barbearia.py
@@ -0,0 +1,12 @@
+class Barbearia:
+    def __init__(self, id, cnpj, nome, endereco, email, telefone, senha):
+        self.id = id
+        self.cnpj = cnpj
+        self.nome = nome
+        self.endereco = endereco
+        self.email = email
+        self.telefone = telefone
+        self.senha = senha
+
+    def __str__(self):
+        return f"Barbearia(id = {self.id}, cnpj={self.cnpj}, nome={self.nome}, endereco={self.endereco}, email=${self.email}, telefone={self.telefone}, senha=${self.senha}"
