bddabb530d1df665ee12637538ae72226e2642f4
rafaelbraf <rafabrunofelix@hotmail.com>
2024-02-16 19:48:04 -0300

    feat - Cria endpoint para inserir Barbearia

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from model.barbearia import Barbearia
 import psycopg2
 
 app = Flask(__name__)
@@ -41,3 +42,35 @@ def get_barbearias():
 
     finally:
         connection.close()
+
+
+@app.route('/barbearias', methods=['POST'])
+def insert_barbearia():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        barbearia: Barbearia = request.json
+        connection = get_db_connection()
+
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO barbearias (cnpj, nome, endereco, email, telefone, senha) VALUES (%s, %s, "
+                               "%s, %s, %s, %s) RETURNING id",
+                               (barbearia['cnpj'], barbearia['nome'], barbearia['endereco'], barbearia['email'], barbearia['telefone'], barbearia['senha']))
+
+                id_nova_barbearia = cursor.fetchone()[0]
+                connection.commit()
+
+            return jsonify({'mensagem': f'Barbearia criada com id {id_nova_barbearia}'}), 201
+
+        except Exception as e:
+            return jsonify({'mensagem': f'Erro ao criar Barbearia: {str(e)}'}), 500
+
+        finally:
+            connection.close()
+
+    else:
+        return 'Content-Type is not supported!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
