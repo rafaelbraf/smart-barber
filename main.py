0a08df7e355d0bbb1e74db5f3ca10fb3b3b8f5ec
rafaelbraf <rafabrunofelix@hotmail.com>
2024-02-16 20:06:16 -0300

    feat - Cria endpoint para buscar Barbearia por id

--- main.py
+++ main.py
@@ -44,6 +44,36 @@ def get_barbearias():
         connection.close()
 
 
+@app.route('/barbearias/<barbearia_id>', methods=['GET'])
+def get_barbearia_by_id(barbearia_id):
+    connection = get_db_connection()
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM barbearias WHERE id = %s", (barbearia_id,))
+            barbearia: Barbearia = cursor.fetchone()
+
+        if barbearia:
+            barbearia_json = {
+                'id': barbearia[0],
+                'cnpj': barbearia[1],
+                'nome': barbearia[2],
+                'endereco': barbearia[3],
+                'email': barbearia[4],
+                'telefone': barbearia[5]
+            }
+
+            return jsonify(barbearia_json)
+
+        return jsonify({"mensagem": "Barbearia não encontrada."})
+
+    except Exception as e:
+        return jsonify({'mensagem': f'Erro ao buscar Barbearia: {str(e)}'}), 500
+
+    finally:
+        connection.close()
+
+
 @app.route('/barbearias', methods=['POST'])
 def insert_barbearia():
     content_type = request.headers.get('Content-Type')
@@ -55,7 +85,8 @@ def insert_barbearia():
             with connection.cursor() as cursor:
                 cursor.execute("INSERT INTO barbearias (cnpj, nome, endereco, email, telefone, senha) VALUES (%s, %s, "
                                "%s, %s, %s, %s) RETURNING id",
-                               (barbearia['cnpj'], barbearia['nome'], barbearia['endereco'], barbearia['email'], barbearia['telefone'], barbearia['senha']))
+                               (barbearia['cnpj'], barbearia['nome'], barbearia['endereco'], barbearia['email'],
+                                barbearia['telefone'], barbearia['senha']))
 
                 id_nova_barbearia = cursor.fetchone()[0]
                 connection.commit()
