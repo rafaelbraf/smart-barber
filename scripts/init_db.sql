12edd65de0acd7ca7c8fb7fd1ba11c7c1291e04c
rafaelbraf <rafabrunofelix@hotmail.com>
2024-02-21 18:43:18 -0300

    refactor - Altera tipo da coluna data_nascimento do Usuário de "DATE" para "VARCHAR"

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     cpf VARCHAR(11) UNIQUE NOT NULL,
     nome VARCHAR(100) NOT NULL,
-    data_nascimento DATE NOT NULL,
+    data_nascimento VARCHAR(10) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     celular VARCHAR(20),
     senha VARCHAR(255) NOT NULL
