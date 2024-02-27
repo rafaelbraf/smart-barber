c2583ef945820ae45deb68c756876d3dcc910047
rafaelbraf <rafabrunofelix@hotmail.com>
2024-02-27 19:48:14 -0300

    feat - Conexões ao Banco de Dados passa a receber parâmetros pelas variáveis de ambiente

--- db/connect_db.py
+++ db/connect_db.py
@@ -1,11 +1,13 @@
+import os
+
 import psycopg2
 
 db_params = {
-    'dbname': 'barbearia-app',
-    'user': 'postgres',
-    'password': 'minha_senha',
-    'host': 'localhost',
-    'port': '5432'
+    'dbname': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASS'),
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT')
 }
 
 
