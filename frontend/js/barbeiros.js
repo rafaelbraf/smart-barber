738ca2296d429bce90fe8774db74fcc30720cb45
rafaelbraf <rafabrunofelix@hotmail.com>
2024-05-01 19:41:25 -0300

    feat/frontend - Implementa cadastro e exclusão de barbeiros

--- frontend/js/barbeiros.js
+++ frontend/js/barbeiros.js
@@ -20,6 +20,8 @@ function buscarBarbeirosDaBarbearia() {
     })
     .then((data) => {
         const container = document.getElementById('barbeiros-container');
+        container.innerHTML = '';
+
         const barbeiros = data.barbeiros;
         barbeiros.forEach(barbeiro => {
             const card = document.createElement('div');
@@ -36,6 +38,16 @@ function buscarBarbeirosDaBarbearia() {
                             <div class="col">
                                 <input type="checkbox" ${barbeiro.ativo ? 'checked' : ''} class="form-check-input" disabled>
                             </div>
+                            <div class="col text-end">
+                                <button class="btn btn-primary btn-sm" onclick="editarBarbeiro(${barbeiro.id})">
+                                    <i class="bi bi-pencil-square"></i>
+                                </button>
+                            </div>
+                            <div class="col text-end">
+                                <button class="btn btn-danger btn-sm" onclick="deletarBarbeiro(${barbeiro.id})">
+                                    <i class="bi bi-trash"></i>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -47,4 +59,81 @@ function buscarBarbeirosDaBarbearia() {
     .catch((error) => {
         console.error('Erro: ', error);
     })
+}
+
+function cadastrarBarbeiroNaBarbearia() {
+    const nomeBarbeiro = document.getElementById('nomeBarbeiro').value;
+    const cpfBarbeiro = document.getElementById('cpfBarbeiro').value;
+    const celularBarbeiro = document.getElementById('celularBarbeiro').value;
+    const emailBarbeiro = document.getElementById('emailBarbeiro').value;
+    const isBarbeiroAdmin = document.getElementById('checkboxBarbeiroAdmin').checked;
+    const isBarbeiroAtivo = document.getElementById('checkboxBarbeiroAtivo').checked;
+    const idBarbearia = localStorage.getItem('barbearia');
+
+    const barbeiro = {
+        nome: nomeBarbeiro,
+        cpf: cpfBarbeiro,
+        celular: celularBarbeiro,
+        email: emailBarbeiro,
+        admin: isBarbeiroAdmin,
+        ativo: isBarbeiroAtivo,
+        idBarbearia: idBarbearia
+    };
+
+    fetch('http://localhost:5000/barbeiros', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(barbeiro)
+    })
+    .then((response) => {
+        if (response.status === 201) {
+            response.json().then(data => {
+                console.log(data);
+
+                limparCamposModalBarbeiro();
+
+                var modal = new bootstrap.Modal(document.getElementById('cadastrarBarbeiroModal'));
+                modal.hide();
+            });
+        } else {
+            throw new Error('Erro ao salvar barbeiro.');
+        }
+    })
+    .catch(error => console.error(error));
+
+    buscarBarbeirosDaBarbearia();
+}
+
+function limparCamposModalBarbeiro() {
+    document.getElementById('nomeBarbeiro').value = '';
+    document.getElementById('cpfBarbeiro').value = '';
+    document.getElementById('celularBarbeiro').value = '';
+    document.getElementById('emailBarbeiro').value = '';
+    document.getElementById('checkboxBarbeiroAdmin').checked = false;
+    document.getElementById('checkboxBarbeiroAtivo').checked = true;
+}
+
+function deletarBarbeiro(barbeiroId) {
+    if (confirm('Tem certeza que deseja excluir o barbeiro?')) {
+        fetch(`http://localhost:5000/barbeiros/${barbeiroId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                alert('Barbeiro excluído com sucesso!');
+                
+                buscarBarbeirosDaBarbearia();
+            } else {
+                throw new Error('Erro ao excluir barbeiro.');
+            }
+        })
+        .catch(error => console.error(error));
+    }
 }
