fdb6ccaa8fb610f25d2299be2da8db8abd3f032e
rafaelbraf <rafabrunofelix@hotmail.com>
2024-05-07 20:51:00 -0300

    [db/scripts] - Altera as colunas barbearia_id para id_barbearia e usuario_id para id_usuario

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS barbeiros (
     email VARCHAR(100) UNIQUE NOT NULL,
     celular VARCHAR(20),
     admin BOOLEAN NOT NULL,
-    barbearia_id INTEGER REFERENCES barbearias(id),
+    id_barbearia INTEGER REFERENCES barbearias(id),
     ativo BOOLEAN NOT NULL
 );
 
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS servicos (
     nome VARCHAR(100) NOT NULL,
     preco NUMERIC(10, 2) NOT NULL,
     tempo_duracao_em_minutos INTEGER NOT NULL,
-    barbearia_id INTEGER REFERENCES barbearias(id),
+    id_barbearia INTEGER REFERENCES barbearias(id),
     ativo BOOLEAN NOT NULL DEFAULT true
 );
 
@@ -46,8 +46,8 @@ CREATE TABLE IF NOT EXISTS agendamentos (
     hora VARCHAR(5) NOT NULL,
     valor_total NUMERIC(10, 2) NOT NULL,
     tempo_duracao_em_minutos INTEGER NOT NULL,
-    barbearia_id INTEGER REFERENCES barbearias(id),
-    usuario_id UUID REFERENCES usuarios(id)
+    id_barbearia INTEGER REFERENCES barbearias(id),
+    id_usuario UUID REFERENCES usuarios(id)
 );
 
 CREATE TABLE IF NOT EXISTS agendamento_servicos (
