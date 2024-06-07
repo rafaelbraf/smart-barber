a6f8a0237a76c9c1e2232d5808643882be71d2f8
LuizDevmax <HenriqueRC09@outlook.com>
2024-06-07 20:54:40 -0300

    [Refac/FrontEnd] Refatorações Front-End

--- frontend/public/js/barbeiros.js
+++ frontend/public/js/barbeiros.js
@@ -1,70 +1,68 @@
+import { ApiClient } from "./utils/apiClient.js";
+import { carregarVariaveisDeAmbiente } from "./utils/loadEnvs.js";
 let urlBackend;
 
-document.addEventListener('DOMContentLoaded', carregarVariaveisDeAmbiente);
+const apiClient = new ApiClient();
+
+document.addEventListener('DOMContentLoaded', async () => {
+    urlBackend = await carregarVariaveisDeAmbiente();
+});
 
 const token = localStorage.getItem('token');
 const barbeariaId = localStorage.getItem('barbearia');
+const buttonCadastrarBarbeiro = document.getElementById('buttonCadastrarBarbeiro')
+buttonCadastrarBarbeiro.addEventListener('click',cadastrarBarbeiro)
 
 
-function buscarBarbeirosDaBarbearia() {
-    fetch(`${urlBackend}/barbeiros/barbearia/${barbeariaId}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
+
+async function buscarBarbeirosDaBarbearia() {
+    const url = `${urlBackend}/barbeiros/barbearia/${barbeariaId}`
+    const headers = {
+        'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`
-        }
-    })
-    .then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('Erro ao buscar barbeiros.');
-        }
-    })
-    .then((data) => {
-        const container = document.getElementById('barbeiros-container');
-        container.innerHTML = '';
+    }
+    const response = await apiClient.get(url,headers)
+    const data = await response.json()
+    const container = document.getElementById('barbeiros-container');
+    container.innerHTML = '';
 
-        const barbeiros = data.result;
-        barbeiros.forEach(barbeiro => {
-            const card = document.createElement('div');
-            card.innerHTML = `
-                <div class="card mt-1" style="cursor: pointer;">
-                    <div class="card-body justify-content-between">
-                        <div class="row">
-                            <div class="col">
-                                ${barbeiro.id}
-                            </div>
-                            <div class="col">
-                                ${barbeiro.nome}
-                            </div>
-                            <div class="col">
-                                <input type="checkbox" ${barbeiro.ativo ? 'checked' : ''} class="form-check-input" disabled>
-                            </div>
-                            <div class="col text-end">
-                                <button class="btn btn-primary btn-sm" onclick="editarBarbeiro(${barbeiro.id})">
-                                    <i class="bi bi-pencil-square"></i>
-                                </button>
-                            </div>
-                            <div class="col text-end">
-                                <button class="btn btn-danger btn-sm" onclick="deletarBarbeiro(${barbeiro.id})">
-                                    <i class="bi bi-trash"></i>
-                                </button>
-                            </div>
+    const barbeiros = data.result;
+    barbeiros.forEach(barbeiro => {
+        const card = document.createElement('div');
+        card.innerHTML = `
+            <div class="card mt-1" style="cursor: pointer;">
+                <div class="card-body justify-content-between">
+                    <div class="row">
+                        <div class="col">
+                            ${barbeiro.id}
+                        </div>
+                        <div class="col">
+                            ${barbeiro.nome}
+                        </div>
+                        <div class="col">
+                            <input type="checkbox" ${barbeiro.ativo ? 'checked' : ''} class="form-check-input" disabled>
+                        </div>
+                        <div class="col text-end">
+                            <button class="btn btn-primary btn-sm" onclick="editarBarbeiro(${barbeiro.id})">
+                                <i class="bi bi-pencil-square"></i>
+                            </button>
+                        </div>
+                        <div class="col text-end">
+                            <button class="btn btn-danger btn-sm" onclick="deletarBarbeiro(${barbeiro.id})">
+                                <i class="bi bi-trash"></i>
+                            </button>
                         </div>
                     </div>
                 </div>
-            `;
+            </div>
+        `;
 
-            container.appendChild(card);
-        });
-    })
-    .catch((error) => {
-        console.error('Erro: ', error);
+        container.appendChild(card);
+    
     })
 }
 
-function cadastrarBarbeiroNaBarbearia() {
+ async function cadastrarBarbeiro() {
     const nomeBarbeiro = document.getElementById('nomeBarbeiro').value;
     const cpfBarbeiro = document.getElementById('cpfBarbeiro').value;
     const celularBarbeiro = document.getElementById('celularBarbeiro').value;
@@ -84,38 +82,24 @@ function cadastrarBarbeiroNaBarbearia() {
             id:idBarbearia
         }
     };
+    const url = `${urlBackend}/barbeiros`
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    }
+    const response = await apiClient.post(url,headers,barbeiro)
+    const data = response.json()
+    if( data.statusCode === 201){
+        limparCamposModalBarbeiro();
 
-    fetch(`${urlBackend}/barbeiros`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(barbeiro)
-    })
-    .then((response) => {
-        if (response.status === 201) {
-            response.json().then(data => {
-
-                limparCamposModalBarbeiro();
-
-                var modal = new bootstrap.Modal(document.getElementById('cadastrarBarbeiroModal'));
-                modal.hide();
-                
-                exibirAlertaBarbeiro("success",data.message);
-                buscarBarbeirosDaBarbearia();
-            });
-        } else if (response.status === 400){
-            response.json().then(data =>{
-                console.log(data)
-                exibirAlertaBarbeiro("error",data.message);
-            })
-        }
-         else {
-            throw new Error('Erro ao salvar barbeiro.');
-        }
-    })
-    .catch(error => console.error(error));
+        var modal = new bootstrap.Modal(document.getElementById('cadastrarBarbeiroModal'));
+        modal.hide();
+        
+        exibirAlertaBarbeiro("success",data.message);
+        buscarBarbeirosDaBarbearia();
+    } else {
+        exibirAlertaBarbeiro("error",data.message);
+    }
 
     buscarBarbeirosDaBarbearia();
 }
@@ -129,25 +113,20 @@ function limparCamposModalBarbeiro() {
     document.getElementById('checkboxBarbeiroAtivo').checked = true;
 }
 
-function deletarBarbeiro(barbeiroId) {
+ async function deletarBarbeiro(barbeiroId) {
     if (confirm('Tem certeza que deseja excluir o barbeiro?')) {
-        fetch(`${urlBackend}/barbeiros/${barbeiroId}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        })
-        .then(response => {
-            if (response.ok) {
-                exibirAlertaBarbeiro("success",'Barbeiro excluído com sucesso!');
-                buscarBarbeirosDaBarbearia();
-            } else {
-                exibirAlertaBarbeiro("error",'Erro ao excluir barbeiro.');
-                throw new Error('Erro ao excluir barbeiro.');
-            }
-        })
-        .catch(error => console.error(error));
+        const url = `${urlBackend}/barbeiros/${barbeiroId}`
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+        const response = await apiClient.delete(url,headers)
+        if (response.ok) {
+            exibirAlertaBarbeiro("success",'Barbeiro excluído com sucesso!');
+            buscarBarbeirosDaBarbearia();
+        } else {
+            exibirAlertaBarbeiro("error",'Erro ao excluir barbeiro.');
+        }
     }
 }
 
@@ -160,25 +139,10 @@ function exibirAlertaBarbeiro(status, message) {
     }
     var alert = document.getElementById(idAlerta);
     alert.classList.add("show");
-    alert.innerHTML = mensagem;
+    alert.innerHTML = message;
     alert.innerHTML +='<button type= "button" class= "btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
     setTimeout(function() {
         alert.classList.remove("show");
     },3000);
 
-}
-function carregarVariaveisDeAmbiente() {
-    fetch('/config')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Erro ao carregar variáveis de ambiente.");
-            }
-            
-            return response.json();
-        })
-        .then(config => {
-            urlBackend = config.apiUrl
-            buscarBarbeirosDaBarbearia();
-        })
-        .catch(error => console.error("Erro ao carregar variáveis de ambiente: ", error));
 }
