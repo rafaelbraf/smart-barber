42eb3e79244269d692d4a68e4254d52dc2f29645
rafaelbraf <rafabrunofelix@hotmail.com>
2024-06-11 09:00:29 -0300

    [fix/backend] Corrige nome das foreign keys nas tabelas barbearias, servicos, barbeiros, clientes e agendamentos

--- backend/src/main/resources/db/migration/V6__cria_tabela_agendamentos.sql
+++ backend/src/main/resources/db/migration/V6__cria_tabela_agendamentos.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS agendamentos (
     valor_total NUMERIC(10, 2) NOT NULL,
     tempo_duracao_em_minutos INTEGER NOT NULL,
     barbearia_id BIGINT REFERENCES barbearias(id),
-    CONSTRAINT fk_barbearia FOREIGN KEY (barbearia_id) REFERENCES barbearias(id),
+    CONSTRAINT fk_barbearia_agendamentos FOREIGN KEY (barbearia_id) REFERENCES barbearias(id),
     cliente_id BIGINT REFERENCES clientes(id),
-    CONSTRAINT fk_cliente FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+    CONSTRAINT fk_cliente_agendamentos FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
