472002153c9c8a3bd83d18d46f52779efbe27a52
rafaelbraf <rafabrunofelix@hotmail.com>
2024-06-12 10:02:29 -0300

    [feat/backend] Implementa endpoint para buscar barbearias pelo nome

--- backend/src/main/java/com/optimiza/clickbarber/controller/BarbeariaController.java
+++ backend/src/main/java/com/optimiza/clickbarber/controller/BarbeariaController.java
@@ -1,9 +1,17 @@
 package com.optimiza.clickbarber.controller;
 
+import com.optimiza.clickbarber.model.Resposta;
+import com.optimiza.clickbarber.model.RespostaUtils;
+import com.optimiza.clickbarber.model.dto.barbearia.BarbeariaDto;
 import com.optimiza.clickbarber.service.BarbeariaService;
+import com.optimiza.clickbarber.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/barbearias")
@@ -14,6 +22,13 @@ public class BarbeariaController {
     @Autowired
     public BarbeariaController(BarbeariaService barbeariaService) {
         this.barbeariaService = barbeariaService;
+    }
+
+    @GetMapping
+    public Resposta<List<BarbeariaDto>> buscarPorNome(@RequestParam(name = "nome") String nome) {
+        var barbearias = barbeariaService.buscarPorNome(nome);
+        var mensagem = Constants.Success.BARBEARIAS_ENCONTRADAS_PELO_NOME + nome;
+        return RespostaUtils.ok(mensagem, barbearias);
     }
 
 }
