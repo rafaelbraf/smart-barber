3ad763bb63c39827a3bcf6d65ebd215995b0878b
rafaelbraf <rafabrunofelix@hotmail.com>
2024-06-13 16:20:50 -0300

    [refactor/backend] Implementa testes unitários para BarbeariaController e BarbeariaService para quando buscar todas as Barbearias

--- backend/src/test/java/com/optimiza/clickbarber/controller/BarbeariaControllerTest.kt
+++ backend/src/test/java/com/optimiza/clickbarber/controller/BarbeariaControllerTest.kt
@@ -2,21 +2,24 @@ package com.optimiza.clickbarber.controller
 
 import com.optimiza.clickbarber.model.dto.barbearia.BarbeariaDto
 import com.optimiza.clickbarber.service.BarbeariaService
+import com.optimiza.clickbarber.utils.Constants
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.data.domain.Pageable
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import java.util.List
 
 @ExtendWith(SpringExtension::class)
 @WebMvcTest(value = [BarbeariaController::class], useDefaultFilters = false)
@@ -34,29 +37,59 @@ internal class BarbeariaControllerTest {
     }
 
     @Test
+    fun buscarTodasAsBarbeariasSemLimite() {
+        val barbearia1 = montarBarbeariaDto()
+        val barbearia2 = montarBarbeariaDto(2, "Barbearia Teste 2")
+        val barbeariasDtoLista = listOf(barbearia1, barbearia2)
+
+        Mockito.`when`(barbeariaService!!.buscarTodos(any(Pageable::class.java))).thenReturn(barbeariasDtoLista)
+
+        mockMvc!!.perform(get("/barbearias")
+            .param("limit", null)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.message").value("${barbeariasDtoLista.size} ${Constants.Success.BARBEARIAS_ENCONTRADAS}"))
+            .andExpect(jsonPath("$.result.[0].id").value(1))
+            .andExpect(jsonPath("$.result.[0].nome").value("Barbearia Teste"))
+            .andExpect(jsonPath("$.result.[1].id").value(2))
+            .andExpect(jsonPath("$.result.[1].nome").value("Barbearia Teste 2"))
+    }
+
+    @Test
+    fun buscarTodasAsBarbeariasComLimite() {
+        val barbearia1 = montarBarbeariaDto()
+        val barbeariasDtoLista = listOf(barbearia1)
+
+        Mockito.`when`(barbeariaService!!.buscarTodos(any(Pageable::class.java))).thenReturn(barbeariasDtoLista)
+
+        mockMvc!!.perform(get("/barbearias")
+            .param("limit", "1")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.message").value("${barbeariasDtoLista.size} ${Constants.Success.BARBEARIAS_ENCONTRADAS}"))
+            .andExpect(jsonPath("$.result.[0].id").value(1))
+            .andExpect(jsonPath("$.result.[0].nome").value("Barbearia Teste"))
+    }
+
+    @Test
     @Throws(Exception::class)
     fun buscarBarbeariaPeloNome() {
         val barbearia1 = montarBarbeariaDto()
         val barbearia2 = montarBarbeariaDto(2L, "Barbearia Teste 2")
-        val barbeariasDtoLista = List.of(barbearia1, barbearia2)
+        val barbeariasDtoLista = listOf(barbearia1, barbearia2)
         Mockito.`when`(barbeariaService!!.buscarPorNome(ArgumentMatchers.anyString())).thenReturn(barbeariasDtoLista)
 
         val nomePesquisar = "Barbearia"
 
-        mockMvc!!.perform(
-            MockMvcRequestBuilders.get("/barbearias")
-                .param("nome", nomePesquisar)
-                .accept(MediaType.APPLICATION_JSON)
-        )
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(
-                MockMvcResultMatchers.jsonPath("$.message")
-                    .value("${Constants.Success.BARBEARIAS_ENCONTRADAS_PELO_NOME} $nomePesquisar")
-            )
-            .andExpect(MockMvcResultMatchers.jsonPath("$.result.[0].id").value(1))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.result.[0].nome").value("Barbearia Teste"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.result.[1].id").value(2))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.result.[1].nome").value("Barbearia Teste 2"))
+        mockMvc!!.perform(get("/barbearias/nome")
+            .param("nome", nomePesquisar)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.message").value("${Constants.Success.BARBEARIAS_ENCONTRADAS_PELO_NOME} $nomePesquisar"))
+            .andExpect(jsonPath("$.result.[0].id").value(1))
+            .andExpect(jsonPath("$.result.[0].nome").value("Barbearia Teste"))
+            .andExpect(jsonPath("$.result.[1].id").value(2))
+            .andExpect(jsonPath("$.result.[1].nome").value("Barbearia Teste 2"))
     }
 
     private fun montarBarbeariaDto(): BarbeariaDto {
